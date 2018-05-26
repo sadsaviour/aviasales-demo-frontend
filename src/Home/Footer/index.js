6f079d3e15330e6ae8abc20562774bd64e5d9a7a
sadsaviour <30339972+sadsaviour@users.noreply.github.com>
2018-05-26 07:00:56 +0300

    feat: stylize footer for mobile

--- src/Home/Footer/index.js
+++ src/Home/Footer/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 
 import instagram from "./img/instagram.svg";
@@ -29,7 +29,14 @@ const Cities = [
   "Лондон"
 ];
 
-const Carriers = ["Air Berlin", "Air France", "Air Baltic", "Emirates,", "KLM"];
+const Carriers = [
+  "Air Berlin",
+  "Air France",
+  "Alitalia",
+  "Air Baltic",
+  "Emirates",
+  "KLM"
+];
 
 const Airports = [
   "Шереметьево",
@@ -49,84 +56,211 @@ const Destinations = [
   "MOW – BKK"
 ];
 
+const Services = [
+  "Горящие авиабилеты",
+  "Календарь низких цен",
+  "Карта низких цен",
+  "Спецпредложения",
+  "Таблица цен",
+  "Блог",
+  "Помощь"
+];
+
+const StyledFooter = styled.div`
+  padding-top: 32px;
+  padding-bottom: 24px;
+`;
+
+const FooterBlockHeader = styled.h2`
+  margin-bottom: 16px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 16px;
+  font-size: 12px;
+
+  color: #4a4a4a;
+`;
+
+const FooterBlockItems = styled.p`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 16px;
+  font-size: 12px;
+
+  color: #5b5b5c;
+`;
+
+const FooterBlockAll = styled.p`
+  margin-top: 16px;
+  margin-bottom: 31px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 16px;
+  font-size: 12px;
+
+  color: #4a4a4a;
+`;
+
+const StyledHr = styled.hr`
+  box-sizing: border-box;
+  size: 1px;
+  color: #e0e6e8;
+`;
+
+const StyledInfoLink = styled.p`
+  margin-top: 4px;
+  margin-bottom: 4px;
+  margin-right: 12px;
+
+  display: inline-block;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 16px;
+  font-size: 12px;
+
+  color: #5b5b5c;
+`;
+
+const SocialIconBlock = styled.div`
+  display: inline-block;
+  margin-right: 18px;
+  margin-top: 8px;
+`;
+
+const SocialIconCapture = styled.p`
+  display: inline;
+
+  margin-left: 6px;
+  margin-top: 0;
+  margin-bottom: 0;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 16px;
+  font-size: 12px;
+
+  color: #4a4a4a;
+`;
+
+const HotelLookLink = styled.p`
+  margin-top: 24px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 16px;
+  font-size: 12px;
+  text-align: center;
+
+  color: #5b5b5c;
+`;
+
+const Copyright = styled.p`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 16px;
+  font-size: 12px;
+
+  color: #5b5b5c;
+`;
+
 function List(props) {
-  return props.list.map(i => <p>{i}</p>);
+  return props.list.map(i => <FooterBlockItems key={i}>{i}</FooterBlockItems>);
 }
 
 export default function Footer(props) {
   return (
-    <div className="Footer">
+    <StyledFooter>
       <div className="container">
         <div className="row">
           <div className="col-xs-6">
-            <h2>СТРАНЫ</h2>
-            <List list={States} />
-            <p>Все страны →</p>
+            <nav>
+              <FooterBlockHeader>СТРАНЫ</FooterBlockHeader>
+              <List list={States} />
+              <FooterBlockAll>Все страны →</FooterBlockAll>
+            </nav>
           </div>
           <div className="col-xs-6">
-            <h2>ГОРОДА</h2>
+            <FooterBlockHeader>ГОРОДА</FooterBlockHeader>
             <List list={Cities} />
-            <p>Все аэропорты →</p>
+            <FooterBlockAll>Все аэропорты →</FooterBlockAll>
           </div>
           <div className="col-xs-6">
-            <h2>АВИАКОМПАНИИ</h2>
+            <FooterBlockHeader>АВИАКОМПАНИИ</FooterBlockHeader>
             <List list={Carriers} />
-            <p>Все авиакомпании →</p>
+            <FooterBlockAll>Все авиакомпании →</FooterBlockAll>
           </div>
           <div className="col-xs-6">
-            <h2>АЭРОПОРТЫ</h2>
+            <FooterBlockHeader>АЭРОПОРТЫ</FooterBlockHeader>
             <List list={Airports} />
-            <p>Все аэропорты →</p>
+            <FooterBlockAll>Все аэропорты →</FooterBlockAll>
           </div>
           <div className="col-xs-6">
-            <h2>НАПРАВЛЕНИЯ</h2>
+            <FooterBlockHeader>НАПРАВЛЕНИЯ</FooterBlockHeader>
             <List list={Destinations} />
           </div>
           <div className="col-xs-6">
-            <h2>СЕРВИСЫ</h2>
-            <p>Горящие авиабилеты</p>
-            <p>Календарь низких цен</p>
-            <p>Карта низких цен</p>
-            <p>Спецпредложения</p>
-            <p>Таблица цен</p>
-            <p>Блог</p>
-            <p>Помощь</p>
+            <FooterBlockHeader>СЕРВИСЫ</FooterBlockHeader>
+            <List list={Services} />
           </div>
         </div>
-        <hr />
-        <div className="row">
-          <p>О компании</p>
-          <p>Партнёрская программа</p>
-          <p>Реклама</p>
-          <p>Вакансии</p>
-          <p>Помощь</p>
-          <p>Правила</p>
-          <p>White Label авиабилеты</p>
-        </div>
-        <div className="row">
-          <img src={vk} alt="VK" />
-          <img src={facebook} alt="Facebook" />
-          <img src={instagram} alt="Instagram" />
-          <img src={twitter} alt="Twitter" />
-          <img src={viber} alt="Viber" />
-        </div>
+      </div>
+      <StyledHr />
+      <div className="container">
+        <nav>
+          <StyledInfoLink>О компании</StyledInfoLink>
+          <StyledInfoLink>Партнёрская программа</StyledInfoLink>
+          <StyledInfoLink>Реклама</StyledInfoLink>
+          <StyledInfoLink>Вакансии</StyledInfoLink>
+          <StyledInfoLink>Помощь</StyledInfoLink>
+          <StyledInfoLink>Правила</StyledInfoLink>
+          <StyledInfoLink>White Label авиабилеты</StyledInfoLink>
+        </nav>
+        <nav>
+          <SocialIconBlock>
+            <img src={vk} alt="VK" />
+            <SocialIconCapture>Вконтакте</SocialIconCapture>
+          </SocialIconBlock>
+          <SocialIconBlock>
+            <img src={facebook} alt="Facebook" />
+            <SocialIconCapture>Фейсбук</SocialIconCapture>
+          </SocialIconBlock>
+          <SocialIconBlock>
+            <img src={instagram} alt="Instagram" />
+            <SocialIconCapture>Инстаграм</SocialIconCapture>
+          </SocialIconBlock>
+          <SocialIconBlock>
+            <img src={twitter} alt="Twitter" />
+            <SocialIconCapture>Твиттер</SocialIconCapture>
+          </SocialIconBlock>
+          <SocialIconBlock>
+            <img src={viber} alt="Viber" />
+            <SocialIconCapture>Вайбер</SocialIconCapture>
+          </SocialIconBlock>
+        </nav>
 
         <div className="row">
           <div className="col-xs-12 col-lg-6">
-            <h3>Поиск и бронирование отелей</h3>
+            <HotelLookLink>Поиск и бронирование отелей</HotelLookLink>
           </div>
         </div>
         <div className="row">
-          <div className="col-xs-12">
+          <div className="col-xs-offset-3 col-xs-6 center-xs">
             <img src={apple} alt="App Store" />
             <img src={google} alt="Google Play Store" />
             <img src={microsoft} alt="Windows Phone" />
           </div>
         </div>
-        <div className="row">
-          <p>© 2007–2018, Aviasales — дешевые авиабилеты</p>
-        </div>
+        <Copyright>© 2007–2018, Aviasales — дешевые авиабилеты</Copyright>
       </div>
-    </div>
+    </StyledFooter>
   );
 }
