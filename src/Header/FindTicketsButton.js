2bc17226302a6b3662c607d10e258d68b7dde113
sadsaviour <30339972+sadsaviour@users.noreply.github.com>
2018-06-03 13:36:20 +0300

    fix:  search button behavior on media size change

--- src/Header/FindTicketsButton.js
+++ src/Header/FindTicketsButton.js
@@ -4,18 +4,23 @@ import { Link } from "react-router-dom";
 
 const Button = styled.button`
   @media only screen and (min-width: 768px) and (max-width: 992px) {
-    flex-grow: 1;
-    flex-basis: 24.2%;
+    flex-grow: 2;
+    flex-basis: 25%;
   }
 
   height: 56px;
+  width: 100%;
 
   @media only screen and (min-width: 992px) {
     height: 58px;
   }
 
   box-sizing: border-box;
-  width: 100%;
+
+  @media only screen and (min-width: 768px) and (max-width: 992px) {
+    padding-left: 18px;
+    padding-right: 18px;
+  }
 
   background: #ff9241;
   border: 0;
@@ -27,6 +32,22 @@ const Button = styled.button`
     border-top-right-radius: 0;
     border-bottom-right-radius: 5px;
   }
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+  font-size: 20px;
+
+  @media only screen and (min-width: 992px) and (max-width: 1260px) {
+    font-size: 18px;
+  }
+
+  text-align: center;
+
+  color: #ffffff;
+
+  cursor: pointer;
 `;
 
 const StyledButtonText = styled.p`
@@ -45,18 +66,12 @@ const StyledButtonText = styled.p`
   color: #ffffff;
 `;
 
-export default class FindTicketsButton extends Component {
-  render() {
-    return (
-      <Button
-        name="Find Tickets"
-        type="submit"
-        className={this.props.className}
-      >
-        <Link style={{ "text-decoration": "none" }} to="/search">
-          <StyledButtonText>Найти билеты</StyledButtonText>
-        </Link>
+export default function FindTicketsButton(props) {
+  return (
+    <Link style={{ "text-decoration": "none" }} to="/search">
+      <Button name="Find Tickets" type="submit" className={props.className}>
+        Найти билеты
       </Button>
-    );
-  }
+    </Link>
+  );
 }
