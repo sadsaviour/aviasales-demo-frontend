8a092232488cb118ad4876a4fa4231ff44a3badd
sadsaviour <30339972+sadsaviour@users.noreply.github.com>
2018-06-13 06:23:03 +0300

    feat: add landing content. mobile layout only

--- src/Landing/bestPrices.js
+++ src/Landing/bestPrices.js
@@ -1,131 +1,157 @@
-const topDestinations = [
+import kr from "./Assets/krasnodar.jpg";
+import bar from "./Assets/barcelona.jpg";
+import minv from "./Assets/minvody.jpg";
+import saintP from "./Assets/saint-p.jpg";
+import simph from "./Assets/simpheropol.jpg";
+import sochy from "./Assets/sochy.jpg";
+
+import ruFlag from "./Assets/ruFlag.svg";
+import mdFlag from "./Assets/mdFlag.png";
+import amFlag from "./Assets/amFlag.svg";
+
+import pobeda from "./Assets/pobeda.png";
+import lufthansa from "./Assets/lufthansa.png";
+import star from "./Assets/starAlliance.png";
+
+import eTicketIcon from "./Assets/tips__eticket.svg";
+import cheapIcon from "./Assets/tips__cheap-flights.svg";
+import adviceIcon from "./Assets/tips__advice.svg";
+
+export const topDestinations = [
   {
     city: "Краснодар",
     country: "РОССИЯ",
     price: "Найти от 1 212 ₽",
-    date: "18 марта"
+    date: "18 марта",
+    img: kr
   },
   {
     city: "Сочи (Адлер)",
     country: "РОССИЯ",
     price: "Найти от 1 334 ₽",
-    date: "27 марта"
+    date: "27 марта",
+    img: sochy
   },
   {
     city: "Санкт-Петербург",
     country: "РОССИЯ",
     price: "Найти от 1 508 ₽",
-    date: "19 февраля"
+    date: "19 февраля",
+    img: saintP
   },
   {
     city: "Минеральные Воды",
     country: "РОССИЯ",
     price: "Найти от 2 074 ₽",
-    date: "13 марта"
+    date: "13 марта",
+    img: minv
   },
   {
     city: "Симферополь (Кр…",
     country: "РОССИЯ",
     price: "Найти от 2 407 ₽",
-    date: "13 марта"
+    date: "13 марта",
+    img: simph
   },
   {
     city: "Барселона",
     country: "ИСПАНИЯ",
     price: "Найти от 4 247 ₽",
-    date: "24 марта"
+    date: "24 марта",
+    img: bar
   }
 ];
 
-const bestPrices = [
+export const bestPricesLastMonth = [
   {
+    flag: ruFlag,
     city: "Симферополь (Крым)",
     country: "КРЫМ",
     prices: [
       {
-        departure: "Из Москвы",
+        origin: "Из Москвы",
         price: "от 4 813 ₽"
       },
       {
-        departure: "Из Санкт-Петербурга",
+        origin: "Из Санкт-Петербурга",
         price: "от 7 857 ₽"
       },
       {
-        departure: "Из Новосибирска",
+        origin: "Из Новосибирска",
         price: "от 15 127 ₽"
       },
       {
-        departure: "Из Екатеринбурга",
+        origin: "Из Екатеринбурга",
         price: "от 9 275 ₽"
       },
       {
-        departure: "Из Челябинска",
+        origin: "Из Челябинска",
         price: "от 9 148 ₽"
       }
     ]
   },
   {
+    flag: amFlag,
     city: "Ереван",
     country: "АРМЕНИЯ",
     prices: [
       {
-        departure: "Из Москвы",
+        origin: "Из Москвы",
         price: "от 6 758 ₽"
       },
       {
-        departure: "Из Санкт-Петербурга",
+        origin: "Из Санкт-Петербурга",
         price: "от 9 932 ₽"
       },
       {
-        departure: "Из Сочи",
+        origin: "Из Сочи",
         price: "от 11 951 ₽"
       },
       {
-        departure: "Из Краснодара",
+        origin: "Из Краснодара",
         price: "от 11 741 ₽"
       },
       {
-        departure: "Из Ростова-на-Дону",
+        origin: "Из Ростова-на-Дону",
         price: "от 11 956 ₽"
       }
     ]
   },
   {
+    flag: mdFlag,
     city: "Кишинёв",
     country: "МОЛДАВИЯ",
     prices: [
       {
-        departure: "Из Москвы",
+        origin: "Из Москвы",
         price: "от 8 319 ₽"
       },
       {
-        departure: "Из Санкт-Петербурга",
+        origin: "Из Санкт-Петербурга",
         price: "от 10 800 ₽"
       },
       {
-        departure: "Из Краснодара",
+        origin: "Из Краснодара",
         price: "от 12 098 ₽"
       },
       {
-        departure: "Из Сургута",
+        origin: "Из Сургута",
         price: "от 16 277 ₽"
       },
       {
-        departure: "Из Нового Уренгоя",
+        origin: "Из Нового Уренгоя",
         price: "от 15 987 ₽"
       }
     ]
   }
 ];
 
-const bestPricesDisclaimer =
-  "Мы знаем, где купить авиабилеты дешево. Билеты на самолет в 220 стран мира. Поиск и сравнение цен на авиабилеты среди 100 агентств и 728 авиакомпаний.";
-
-const specialOffers = [
+export const specialOffers = [
   {
     offerHighlight: "Билеты от 499 рублей!",
-    alliance: false,
+    allianceLogo: false,
     carrier: "Pobeda",
+    logo: pobeda,
     price: "499 ₽",
     expirationTime: "Осталось 45 дней",
     offerText:
@@ -133,8 +159,9 @@ const specialOffers = [
   },
   {
     offerHighlight: "В Нью-Йорк от 20 680 ₽",
-    alliance: true,
+    allianceLogo: star,
     carrier: "Lufthansa",
+    logo: lufthansa,
     price: "20 680 ₽",
     expirationTime: "Осталось 19 дней",
     offerText:
@@ -142,8 +169,9 @@ const specialOffers = [
   },
   {
     offerHighlight: "В Лос-Анджелес от…",
-    alliance: true,
+    allianceLogo: star,
     carrier: "Lufthansa",
+    logo: lufthansa,
     price: "20 360 ₽",
     expirationTime: "Осталось 19 дней",
     offerText:
@@ -151,23 +179,23 @@ const specialOffers = [
   }
 ];
 
-const tipsText = [
+export const tipsText = [
   {
-    icon: "",
+    icon: cheapIcon,
     heading: "КАК КУПИТЬ ДЕШЕВЫЕ АВИАБИЛЕТЫ?",
     text:
       "Aviasales.ru — это метапоисковик авиабилетов. Мы ищем билеты на самолет по сотням авиакомпаний и находим за считанные минуты самые дешевые авиабилеты. Чтобы купить авиабилет дешево, воспользуйтесь нашим поиском, который совершенно бесплатно сравнивает цены на авиабилеты онлайн и находит самые дешевые перелеты. Больше нет смысла ходить в авиакассы, обзванивать агентства — дешевый билет находится на расстоянии клика. На нашем сайте вы можете подобрать дешевые билеты на самолет в Европу, Азию и на другие континенты. Мы написали для вас простую инструкцию о том, как пользоваться поиском и экономить на перелетах от 1000 до 20 000 руб в год.",
     link: "Подробнее"
   },
   {
-    icon: "",
+    icon: eTicketIcon,
     heading: "ЭЛЕКТРОННЫЙ АВИАБИЛЕТ",
     text:
       "Электронный авиабилет — это, по сути, обычный билет на самолет, но только в менее привычном для путешественника виде. Вся информация об авиаперелете (данные пассажира, маршрут следования) хранится в электронной базе, а пассажир получает на руки маршрут-квитанцию. Некоторые пассажиры, купив авиабилет онлайн и получив маршрут-квитанцию, удивлены ее видом — это обыкновенный лист формата А4, на котором распечатана вся информация о предстоящем перелете. Однако это действительно официальный документ, подтверждающий договор между авиаперевозчиком и пассажиром. При регистрации на рейс пассажир должен предъявить маршрут-квитанцию вместе с удостоверением личности точно так же, как предъявляют обыкновенный бумажный билет. Следует отметить, что электронный билет, приобретенный онлайн, стоит дешевле своего бумажного аналога.",
     link: "Подробнее"
   },
   {
-    icon: "",
+    icon: adviceIcon,
     heading: "20 СОВЕТОВ АВИАПУТЕШЕСТВЕННИКАМ",
     text:
       "Есть масса путеводителей по странам, но ни одного о том, как провести время в самолете. Для того, чтобы сделать ваш перелет максимально комфортным, мы написали 20 советов о подготовке к путешествию. Ведь настоящее путешествие начинается со слов «Добро пожаловать на борт нашего самолета»!",
--- src/Landing/index.js
+++ src/Landing/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
+import {
+  topDestinations,
+  bestPricesLastMonth,
+  specialOffers,
+  tipsText
+} from "./bestPrices.js";
+
 import compassIcon from "./Assets/compasIcon.svg";
 import editIcon from "./Assets/editIcon.svg";
 import anywhereIcon from "./Assets/destination__anywhere.svg";
@@ -10,6 +17,14 @@ import nightlifeIcon from "./Assets/destination__nightlife.svg";
 import shoppingIcon from "./Assets/destination__shopping.svg";
 import sunIcon from "./Assets/destination__sun.svg";
 
+import appDemo from "./Assets/ios-app-demo.png";
+import rating from "./Assets/rating.svg";
+import apple from "./Assets/apple.svg";
+import android from "./Assets/android.svg";
+import wf from "./Assets/wf.svg";
+
+import bestPricesCalendarIcon from "./Assets/bestPricesCalendarIcon.svg";
+
 const StyledMain = styled.main`
   padding-top: 40px;
 `;
@@ -18,12 +33,7 @@ const CompassIcon = ({ className }) => {
   return <img className={className} src={compassIcon} alt={"compass"} />;
 };
 
-const CompassIconWrapper = styled(CompassIcon)`
-  margin-left: auto;
-  margin-right: auto;
-`;
-
-const PopularDestinationsContainer = styled.div`
+const PopularDestinationsHeader = styled.div`
   margin-top: 24px;
 
   text-align: center;
@@ -87,22 +97,583 @@ const DestinationsList = () => {
   );
 };
 
+const PopularDestinationsCitiesList = styled.div`
+  font-family: Roboto;
+  font-style: normal;
+`;
+
+const PopularDestinationCity = styled.div`
+  margin-bottom: 16px;
+
+  padding-bottom: 12px;
+
+  background: #ffffff;
+  box-shadow: 0px 2px 12px rgba(0, 75, 93, 0.12);
+  border-radius: 8px;
+`;
+
+const PopularDestinationCityLabel = styled.div`
+  margin-top: 16px;
+  margin-left: 16px;
+
+  font-weight: bold;
+  line-height: 20px;
+  font-size: 16px;
+
+  color: #5b5b5c;
+`;
+
+const PopularDestinationCountryLabel = styled.div`
+  margin-top: 4px;
+  margin-left: 16px;
+
+  font-weight: 500;
+  line-height: 20px;
+  font-size: 12px;
+
+  color: #a0b0b9;
+`;
+
+const PopularDestinationPrice = styled.div`
+  margin-top: 16px;
+  margin-right: 16px;
+
+  font-weight: normal;
+  line-height: normal;
+  font-size: 14px;
+  text-align: right;
+
+  color: #00bae8;
+`;
+
+const PopularDestinationDate = styled.div`
+  margin-top: 4px;
+  margin-right: 16px;
+
+  font-weight: 500;
+  line-height: 20px;
+  font-size: 12px;
+  text-align: right;
+
+  color: #a0b0b9;
+`;
+
+const PopularDestinationImage = styled.img`
+  width: 100%;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+`;
+
+const PopularDestinationsCities = () => {
+  return (
+    <PopularDestinationsCitiesList>
+      {topDestinations.map(d => (
+        <PopularDestinationCity key={d.city}>
+          <PopularDestinationImage src={d.img} alt={d.city} />
+          <div className="container">
+            <div className="row between-xs">
+              <PopularDestinationCityLabel>
+                {d.city}
+              </PopularDestinationCityLabel>
+              <PopularDestinationPrice>{d.price}</PopularDestinationPrice>
+            </div>
+            <div className="row between-xs">
+              <PopularDestinationCountryLabel>
+                {d.country}
+              </PopularDestinationCountryLabel>
+              <PopularDestinationDate>{d.date}</PopularDestinationDate>
+            </div>
+          </div>
+        </PopularDestinationCity>
+      ))}
+    </PopularDestinationsCitiesList>
+  );
+};
+
+const BestPricesCalendarIcon = ({ className }) => {
+  return (
+    <img className={className} src={bestPricesCalendarIcon} alt={"calendar"} />
+  );
+};
+
+const BestPricesCalendarHeader = styled.div`
+  margin-top: 24px;
+
+  text-align: center;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+  font-size: 18px;
+  text-align: center;
+
+  color: #4a4a4a;
+`;
+
+const BestPricesContainer = styled.div`
+  padding-top: 24px;
+  padding-bottom: 8px;
+
+  font-family: Roboto;
+  font-style: normal;
+
+  border-bottom: 0.4px dashed #afbec6;
+`;
+
+const BestPricesCity = styled.div`
+  font-weight: bold;
+  line-height: 32px;
+  font-size: 22px;
+
+  color: #5b5b5c;
+`;
+const BestPricesCountry = styled.div`
+  font-weight: 500;
+  line-height: 20px;
+  font-size: 12px;
+
+  color: #a0b0b9;
+`;
+const BestPricesOrigin = styled.div`
+  margin-bottom: 16px;
+
+  font-weight: normal;
+  line-height: 20px;
+  font-size: 16px;
+
+  color: #4a4a4a;
+`;
+const BestPricesPrice = styled.div`
+  font-weight: normal;
+  line-height: 20px;
+  font-size: 16px;
+
+  color: #00bae8;
+`;
+
+const BestPricesCityContainer = styled.div`
+  display: inline-block;
+  margin-bottom: 24px;
+`;
+
+const BestPricesFlag = styled.img`
+  display: inline-block;
+`;
+
+const BestPricesCapture = styled.div`
+  margin-top: 32px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 24px;
+  font-size: 16px;
+  text-align: center;
+
+  color: #4a4a4a;
+`;
+
+const BestPricesDisclaimer = styled.div`
+  margin-top: 24px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 20px;
+  font-size: 14px;
+  text-align: center;
+
+  color: #a0b0b9;
+`;
+
+const SpecialOffersListContainer = styled.div`
+  padding-top: 16px;
+  padding-left: 6px;
+  padding-right: 6px;
+  padding-bottom: 24px;
+
+  background: linear-gradient(162.96deg, #00b0de 41.41%, #196ebd 141.41%);
+`;
+
+const SpecialOffersListHeader = styled.h2`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  font-size: 28px;
+
+  color: #ffffff;
+`;
+
+const SpecialOfferContainer = styled.div`
+  box-sizing: border-box;
+  height: 270px;
+
+  margin-bottom: 12px;
+
+  padding-bottom: 16px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 16px;
+  font-size: 12px;
+
+  color: #242424;
+
+  background: #ffffff;
+`;
+
+const SpecialOfferHeader = styled.div`
+  position: relative;
+
+  background: #cd2027;
+
+  padding-top: 22px;
+  padding-left: 16px;
+  padding-bottom: 24px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 20px;
+  font-size: 16px;
+
+  color: #ffffff;
+`;
+
+const SpecialOfferHeaderAllianceLogo = styled.img`
+  position: absolute;
+
+  top: 12px;
+  right: 16px;
+`;
+
+const SpeciaOfferContent = styled.div`
+  height: 191px;
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+`;
+
+const SpecialOfferPrice = styled.div`
+  margin-top: 18px;
+  margin-right: 16px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 16px;
+  font-size: 12px;
+  text-align: right;
+
+  color: #5c5c5c;
+
+  .strong {
+    font-size: 20px;
+  }
+`;
+
+const SpecialOfferLogoAndPriceRow = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+`;
+
+const SpecialOfferShowMoreButton = styled.div`
+  padding-top: 10px;
+  padding-bottom: 10px;
+
+  margin-left: 8px;
+  margin-right: 8px;
+
+  background: rgba(0, 0, 0, 0.0001);
+  border: 2px solid #cd1f27;
+  border-radius: 3px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 20px;
+  font-size: 16px;
+  text-align: center;
+
+  color: #d93533;
+`;
+
+const SpecialOfferLogo = styled.img`
+  margin-top: 22px;
+  margin-left: 8px;
+`;
+
+const SpecialOffersListFooter = styled.div`
+  margin-top: 32px;
+  margin-bottom: 24px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 20px;
+  font-size: 16px;
+  text-align: center;
+
+  color: #ffffff;
+`;
+
+const TipsHeader = styled.h2`
+  margin-top: 22px;
+  margin-left: 6px;
+  margin-right: 6px;
+  margin-bottom: 20px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  line-height: 18px;
+  font-size: 13px;
+
+  color: #4a4a4a;
+`;
+const TipsIcon = styled.img`
+  margin-right: 10px;
+`;
+const TipsText = styled.div`
+  margin-left: 6px;
+  margin-right: 6px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 20px;
+  font-size: 14px;
+
+  color: #4a4a4a;
+`;
+const TipsShowMore = styled.span`
+  margin-left: 10px;
+  color: #00ace2;
+`;
+
+const AppAdContainer = styled.div`
+  position: relative;
+
+  margin-top: 24px;
+
+  padding-top: 24px;
+
+  height: 348px;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  line-height: 24px;
+  font-size: 14px;
+
+  color: #ffffff;
+
+  background: linear-gradient(137.4deg, #00b0de 4.18%, #196ebd 104.18%);
+`;
+
+const AppAdHeader = styled.h2`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  font-size: 24px;
+  text-align: center;
+
+  color: #ffffff;
+`;
+
+const AppAdRow = styled.div`
+  text-align: center;
+`;
+
+const AppAdLinksContainer = styled.div`
+  position: absolute;
+
+  bottom: 37px;
+  right: 20px;
+
+  display: inline-block;
+
+  vertical-align: top;
+`;
+
+const AppAdLink = styled.div`
+  margin-bottom: 20px;
+
+  text-align: left;
+`;
+
+const AppAdImage = styled.img`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+`;
+
+const AppAdStoreIcon = styled.img`
+  display: inline-block;
+
+  margin-right: 8px;
+
+  vertical-align: top;
+`;
+
+const AppAd = () => (
+  <AppAdContainer>
+    <AppAdHeader>Скачай мобильное приложение Aviasales.ru</AppAdHeader>
+    <AppAdRow>
+      <img
+        src={rating}
+        alt="rating"
+        style={{ display: "inline-block", marginRight: "12px" }}
+      />
+      <span>Более 103 000 оценок</span>
+    </AppAdRow>
+    <div>
+      <AppAdImage src={appDemo} alt="app screenshot" />
+      <AppAdLinksContainer>
+        <AppAdLink>
+          <AppAdStoreIcon src={apple} alt="App Store" />
+          iPhone или iPad
+        </AppAdLink>
+        <AppAdLink>
+          <AppAdStoreIcon src={android} alt="Google Play Store" />Android
+        </AppAdLink>
+        <AppAdLink>
+          <AppAdStoreIcon src={wf} alt="Windows Phone" />Windows Phone
+        </AppAdLink>
+      </AppAdLinksContainer>
+    </div>
+  </AppAdContainer>
+);
+
+const Tips = () => {
+  return (
+    <div>
+      {tipsText.map(t => (
+        <section>
+          <TipsHeader>
+            <TipsIcon src={t.icon} alt={"icon"} />
+            {t.heading}
+          </TipsHeader>
+          <TipsText>
+            {t.text}
+            <TipsShowMore>Подробнее</TipsShowMore>
+          </TipsText>
+        </section>
+      ))}
+    </div>
+  );
+};
+
+const SpecialOffersList = () => {
+  return (
+    <SpecialOffersListContainer>
+      <SpecialOffersListHeader>
+        Спецпредложения на авиабилеты
+      </SpecialOffersListHeader>
+      {specialOffers.map(o => (
+        <SpecialOfferContainer key={o.offerHighlight}>
+          <SpecialOfferHeader>
+            {o.offerHighlight}
+            {o.allianceLogo && (
+              <SpecialOfferHeaderAllianceLogo
+                src={o.allianceLogo}
+                alt={"alliance"}
+              />
+            )}
+          </SpecialOfferHeader>
+          <SpeciaOfferContent>
+            <SpecialOfferLogoAndPriceRow>
+              <SpecialOfferLogo src={o.logo} alt={o.carrier} />
+              <SpecialOfferPrice>
+                от <strong>{o.price}</strong>
+                <div style={{ color: "#D93633" }}>{o.expirationTime}</div>
+              </SpecialOfferPrice>
+            </SpecialOfferLogoAndPriceRow>
+            <div style={{ marginLeft: "8px" }}>{o.offerText}</div>
+            <SpecialOfferShowMoreButton>
+              Узнать подробности
+            </SpecialOfferShowMoreButton>
+          </SpeciaOfferContent>
+        </SpecialOfferContainer>
+      ))}
+      <SpecialOffersListFooter>
+        <div style={{ "text-decoration": "underline" }}>
+          {" "}
+          Смотреть все спецпредложения
+        </div>
+        <div>* средняя цена по направлению</div>
+      </SpecialOffersListFooter>
+    </SpecialOffersListContainer>
+  );
+};
+
+const BestPricesLastMonth = () => {
+  return (
+    <div className="container">
+      {bestPricesLastMonth.map(c => (
+        <BestPricesContainer key={c.city}>
+          <BestPricesFlag src={c.flag} alt={c.country} />
+          <BestPricesCityContainer>
+            <BestPricesCity>{c.city}</BestPricesCity>
+            <BestPricesCountry>{c.country}</BestPricesCountry>
+          </BestPricesCityContainer>
+          <div className="col-xs-12">
+            {c.prices.map(p => (
+              <div key={p.origin} className="row between-xs">
+                <BestPricesOrigin>{p.origin}</BestPricesOrigin>
+                <BestPricesPrice>{p.price}</BestPricesPrice>
+              </div>
+            ))}
+          </div>
+        </BestPricesContainer>
+      ))}
+      <div className="row center-xs">
+        <BestPricesCapture>
+          Мы знаем, где купить авиабилеты дешево. Билеты на самолет в 220 стран
+          мира. Поиск и сравнение цен на авиабилеты среди 100 агентств и 728
+          авиакомпаний.
+        </BestPricesCapture>
+        <BestPricesDisclaimer>
+          Цены, найденные пользователями за последние 48 часов, не являются
+          офертой.
+        </BestPricesDisclaimer>
+      </div>
+    </div>
+  );
+};
+
 export default function Main(props) {
   return (
     <StyledMain>
       <div className="container">
         <div className="row center-xs">
-          <CompassIconWrapper />
+          <CompassIcon />
         </div>
-        <PopularDestinationsContainer>
+        <PopularDestinationsHeader>
           Популярные направления <br /> перелетов из города
           <br />
           <span style={{ color: "#00ACE2" }}>Москва</span>
           <EditCityIcon src={editIcon} alt={"edit"} />
-        </PopularDestinationsContainer>
+        </PopularDestinationsHeader>
         <div className="row center-xs">
           <DestinationsList />
         </div>
+        <PopularDestinationsCities />
+        <div className="row center-xs">
+          <BestPricesCalendarIcon />
+        </div>
+        <BestPricesCalendarHeader>
+          Лучшие цены на авиабилеты за последний месяц
+        </BestPricesCalendarHeader>
+        <BestPricesLastMonth />
+        <SpecialOffersList />
+        <Tips />
+        <AppAd />
       </div>
     </StyledMain>
   );
