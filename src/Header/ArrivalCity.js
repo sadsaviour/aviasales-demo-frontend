fa31d2cbcc60f46167582fdb1a321cc7770bbaa5
sadsaviour <30339972+sadsaviour@users.noreply.github.com>
2018-07-14 15:23:16 +0300

    fix: remove constructor

--- src/Header/ArrivalCity.js
+++ src/Header/ArrivalCity.js
@@ -122,53 +122,46 @@ const IATACode = styled.div`
   color: #a0b0b9;
 `;
 
+const airports = [
+  { name: "Бангкок", country: "Таиланд", iataCode: "BKK" },
+  { name: "Барселона", country: "Испания", iataCode: "BCN" },
+  { name: "Баландино", country: "Челябинск", iataCode: "CEK" },
+  { name: "Бандаранаике", country: "Коломбо", iataCode: "CMB" },
+  { name: "Батуми", country: "Грузия", iataCode: "CMB" },
+  { name: "Денпасар Бали", country: "Индонезия", iataCode: "DPS" },
+  { name: "Франкфурт-на-Майне", country: "Германия", iataCode: "FRA" },
+  { name: "Манила", country: "Филипинны", iataCode: "MNL" },
+  { name: "Мале", country: "Мальдивы", iataCode: "MLE" },
+  { name: "Мюнхен", country: "Германия", iataCode: "MUC" },
+  { name: "Минеральные Воды", country: "Россия", iataCode: "MRV" },
+  { name: "Мальта", country: "Мальта", iataCode: "MLA" },
+  { name: "Москва", country: "Россия", iataCode: "VKO" }
+];
+
 class ArrivalCity extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dropdownVisible: false,
+  state = {
+    dropdownVisible: false
+  };
 
-      airports: [
-        { name: "Бангкок", country: "Таиланд", iataCode: "BKK" },
-        { name: "Барселона", country: "Испания", iataCode: "BCN" },
-        { name: "Баландино", country: "Челябинск", iataCode: "CEK" },
-        { name: "Бандаранаике", country: "Коломбо", iataCode: "CMB" },
-        { name: "Батуми", country: "Грузия", iataCode: "CMB" },
-        { name: "Денпасар Бали", country: "Индонезия", iataCode: "DPS" },
-        { name: "Франкфурт-на-Майне", country: "Германия", iataCode: "FRA" },
-        { name: "Манила", country: "Филипинны", iataCode: "MNL" },
-        { name: "Мале", country: "Мальдивы", iataCode: "MLE" },
-        { name: "Мюнхен", country: "Германия", iataCode: "MUC" },
-        { name: "Минеральные Воды", country: "Россия", iataCode: "MRV" },
-        { name: "Мальта", country: "Мальта", iataCode: "MLA" },
-        { name: "Москва", country: "Россия", iataCode: "VKO" }
-      ]
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleFocus = this.handleFocus.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleFocus(event) {
+  handleFocus = event => {
     this.setState({ dropdownVisible: true });
-  }
+  };
 
-  handleChange(event) {
+  handleChange = event => {
     this.props.updateDestination(event.target.value);
-  }
+  };
 
-  handleClick(event) {
+  handleClick = event => {
     this.props.updateDestination(event.name, event.iataCode);
     this.setState({ dropdownVisible: false });
-  }
+  };
 
-  handleClickOutside = evt => {
+  handleClickOutside = event => {
     this.setState({ dropdownVisible: false });
   };
 
   render() {
-    const airportSuggestionsList = this.state.airports.map(a => (
+    const airportSuggestionsList = airports.map(a => (
       <Suggestion
         key={a.name}
         style={{ cursor: "pointer" }}
